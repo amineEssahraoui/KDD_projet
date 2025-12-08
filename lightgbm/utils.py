9d9b369745ef26033caf4a7023a52096646ce910
Amine Essahraoui <amine.essahraoui04@gmail.com>
2025-12-08 18:57:32 +0100

    Creat check_X_y function

--- lightgbm/utils.py
+++ lightgbm/utils.py
@@ -18,7 +18,7 @@ def ValidateInputData(X):
     Raises:
         ValueError si X invalide
     """
-    
+
     if isinstance(X, pd.DataFrame):
         X = X.values
     elif isinstance(X, pd.Series):
@@ -30,3 +30,33 @@ def ValidateInputData(X):
         raise ValueError("Input data must be 2-dimensional.")
     
     return X
+
+def check_X_y(X, y):
+    """
+    Vérifie que X et y ont des dimensions compatibles
+    
+    Args:
+        X : array-like, shape (n_samples, n_features)
+        y : array-like, shape (n_samples,)
+        
+    Returns:
+        None
+        
+    Raises:
+        ValueError si les dimensions ne sont pas compatibles
+    """
+    
+    X = ValidateInputData(X)
+    y = np.asarray(y)
+    
+    if X.shape[0] != y.shape[0]:
+        raise ValueError("Number of samples in X and y must be equal.")
+    
+    if X.shape[0] == 0 or y.shape[0] == 0:
+        raise ValueError("X and y cannot be empty.")
+    
+    if y.shape == ():
+        raise ValueError("y must be a 1-dimensional array.")
+    
+    return X, y
+
