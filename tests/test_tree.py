db3f74649c55a91844ffab490788f0ced19f789d
Amine Essahraoui <amine.essahraoui04@gmail.com>
2025-12-12 00:17:23 +0100

    Creating a simple function for check if a tree can fit !

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,3 +1,7 @@
 import numpy as np
 from lightgbm import DecisionTree
 
+def test_tree_can_fit_sample_data():
+    X = np.array([[1,2] , [3,4], [5,6], [7,8]])
+    gradients = np.array([0.5, -0.3, 0.2, -0.1])
+    hessians = np.ones(4)
