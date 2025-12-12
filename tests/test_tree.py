d69b71b5a2f489c14d3e11857729b5698c1e73e4
Amine Essahraoui <amine.essahraoui04@gmail.com>
2025-12-12 01:07:49 +0100

    Another debuging

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -14,7 +14,7 @@ def test_tree_can_fit_sample_data():
         min_gain_to_split=0.0
     )
     tree.fit(X, gradients, hessians)
-    assert tree.root_node is not None
+    assert tree.root is not None
     assert tree.n_features_ == 2
 
 def test_tree_leaf_wise_creates_limited_leaves():
@@ -27,7 +27,7 @@ def test_tree_leaf_wise_creates_limited_leaves():
         max_depth=10,
         num_leaves=num_leaves_target,
         min_data_in_leaf=1,
-        main_sum_hessian_in_leaf=0.0,
+        min_sum_hessian_in_leaf=0.0,
         lambda_l2=0.1,
         min_gain_to_split=0.0
     )
@@ -58,7 +58,7 @@ def test_tree_predict_returns_correct_shape():
     assert predictions.shape == (50,)
     assert np.all(np.isfinite(predictions))
 
-def tree_can_handles_constant_features():
+def test_tree_can_handles_constant_features():
     X = np.array([[1, 5], [1, 6], [1, 7], [1, 8]])
     gradients = np.array([0.5, -0.3, 0.2, -0.1])
     hessians = np.ones(4)
