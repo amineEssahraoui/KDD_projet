ce367a8a3da46c579c5e5f7661d74b74d83cba49
Amine Essahraoui <amine.essahraoui04@gmail.com>
2025-12-12 16:27:38 +0100

    A simple data (numpy array) to check if it accept it

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,4 +3,7 @@ import pandas as pd
 from lightgbm.utils import ValidateInputData, check_X_y, check_is_fitted, validate_sample_weight
 
 def test_validate_input_accepts_numpy(): 
-    
+    X = np.array([[1, 2], [3, 4]])
+    result = ValidateInputData(X)
+    assert isinstance(result.data, np.ndarray)
+    assert result.shape == (2, 2)
